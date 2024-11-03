fcd7e6a34b5ea0b803b02f7b810bd1a6c50b6501
iambabazada <iambabazada@gmail.com>
2024-11-04 03:35:10 +0400

    hero section

--- camp-traveling/components/Hero/Hero.jsx
+++ camp-traveling/components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Button } from "../ui/button";
 import { ArrowBigLeftIcon, ArrowDownUp, ArrowRightIcon } from "lucide-react";
 
+
 const Hero = () => {
   return (
     <div className="container mx-auto mt-5">
